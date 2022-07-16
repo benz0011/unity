8f243d9eda98af0359ee39bf90ad6c699e92352a
benz0011 <108708248+benz0011@users.noreply.github.com>
2022-07-16 19:00:51 +0700

    Update

--- Assets/Scripts/Gameplay.cs
+++ Assets/Scripts/Gameplay.cs
@@ -1,33 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using System;
 
-public class Gameplay : MonoBehaviour
+public class GamePlay : MonoBehaviour
 {
-    Int counter=0;
-   
-    
-    
+    public TMP_Text Text1;
+    private int counter;
+    void Start()
+    {
+         counter = 0;
+    }
     void Update()
     {
+        Text1.text = "Score:" + counter;
         if (Input.GetMouseButtonDown(0))
-        
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
-            if (Physics.Raycast(ray,out hit))
+            if (Physics.Raycast(ray, out hit))
             {
                 if (hit.collider.gameObject.name.CompareTo("RedSphere") == 0)
+                counter = counter - 20;
                 {
                     hit.collider.gameObject.SetActive(false);
                 }
-           
-                if (hit.collider.gameObject.name.CompareTo("GreenSphere") == 0)
-                {
-                    hit.collider.gameObject.SetActive(false);
-                }
-            
                 if (hit.collider.gameObject.name.CompareTo("BlueSphere") == 0)
+                counter = counter + 10;
+                {
+                    hit.collider.gameObject.SetActive(false);
+                }
+                if (hit.collider.gameObject.name.CompareTo("GreenSphere") == 0)
+                counter = counter + 20;
                 {
                     hit.collider.gameObject.SetActive(false);
                 }
